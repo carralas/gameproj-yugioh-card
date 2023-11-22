7df90a6f7be51095fc73cf0ea49ad42c56b6c3b3
lucas! <carralas@icloud.com>
2023-11-22 17:17:20 -0300

    feat: create cards data

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -14,12 +14,33 @@ const state = {
         playerScore: 0,
         enemyScore: 0
     },
-    /* actions - variáveis que cotrolam ações na engine */
-    actions: {
-    },
-    /* objects - variáveis que carregam informações externas */
-    objects: {
-    }
+    /* cards - variáveis que cotrolam armazenam as cartas */
+    cards: [
+        {
+            id: 0,
+            cardName: 'BEW Dragon',
+            cardType: 'Fighter',
+            img: '../img/dragon',
+            strong: 'Mage',
+            weak: 'Tank'
+        },
+        {
+            id: 1,
+            cardName: 'B Mage',
+            cardType: 'Mage',
+            img: '../img/magician',
+            strong: 'Tank',
+            weak: 'Fighter'
+        },
+        {
+            id: 2,
+            cardName: 'Exodia',
+            cardType: 'Tank',
+            img: '../img/exodia',
+            strong: 'Fighter',
+            weak: 'Mage'
+        }
+    ]
 }
 
 function main() {
