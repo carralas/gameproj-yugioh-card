4044d20504e69ec8e4091248bd8373a99f34d353
lucas! <carralas@icloud.com>
2023-11-22 17:05:13 -0300

    feat: create game state

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,9 +1,18 @@
 const state = {
     /* views - variáveis para exibição na tela */
     view: {
+        scoreBox: document.querySelector('#points'),
+        cardInfoFace: document.querySelector('#card-image'),
+        cardInfoName: document.querySelector('#card-name'),
+        cardInfoType: document.querySelector('#card-type'),
+        cardBattlePlayer: document.querySelector('#player-card'),
+        cardBattleEnemy: document.querySelector('#enemy-card'),
+        button: document.querySelector('#advance')
     },
     /* values - variáveis para controle interno */
     values: {
+        playerScore: 0,
+        enemyScore: 0
     },
     /* actions - variáveis que cotrolam ações na engine */
     actions: {
