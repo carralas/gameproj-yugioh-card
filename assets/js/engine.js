3d4308583934134fb25edf04e37fd406a6c6e919
lucas! <carralas@icloud.com>
2023-11-22 15:00:10 -0300

    feat: create engine js code file

---
+++ assets/js/engine.js
@@ -0,0 +1,19 @@
+const state = {
+    /* views - variáveis para exibição na tela */
+    view: {
+    },
+    /* values - variáveis para controle interno */
+    values: {
+    },
+    /* actions - variáveis que cotrolam ações na engine */
+    actions: {
+    },
+    /* objects - variáveis que carregam informações externas */
+    objects: {
+    }
+}
+
+function main() {
+}
+
+main();
